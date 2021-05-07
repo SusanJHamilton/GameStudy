cf79f75efadfae587e16a627a0ac42bd91f39af0
RobertBHamilton <robert.bryan.hamilton@gmail.com>
2021-05-07 12:35:52 -0500

    Commit new files

--- ItemFactory.java
+++ ItemFactory.java
@@ -1,15 +1,15 @@
 import java.util.HashMap;
-
+/* Creates all items found in the game */
 public class ItemFactory{
   public HashMap<String,Item> items;
-
+  
   public ItemFactory(){
-	makeItems();
+    makeItems();
   }
   public Item getItem(String name){
-         Item x= items.get(name);
-	 if (x==null) {return new Item("unknown",0,"not an item in this game");}
-	 return x;
+    Item x= items.get(name);
+    if (x==null) {return new Item("unknown",0,"not an item in this game");}
+    return x;
   }
   private void makeItems(){
     items = new HashMap<String,Item>();
@@ -18,19 +18,19 @@ public class ItemFactory{
     items.put("mud",new Item("mud",  0.52,"cheap to make and sell"));
   }
   public Item merge(String x,String y){
-	return merge(getItem(x),getItem(y));
+    return merge(getItem(x),getItem(y));
   }
   public Item merge(Item x,Item y){
-     if( x.name.equals("dirt") && y.name.equals("water")) return getItem("mud");
-     if( y.name.equals("dirt") && x.name.equals("water")) return getItem("mud");
-      return null;
+    if( x.name.equals("dirt") && y.name.equals("water")) return getItem("mud");
+    if( y.name.equals("dirt") && x.name.equals("water")) return getItem("mud");
+    return null;
   }
- public static void main(String[] args){
-   ItemFactory f=new ItemFactory();
-   Item x=f.getItem(args[0]);
-   System.out.println(x.name+" is "+x.description+" and can be had for a mere "+x.price);
-   Item merged=f.merge(args[0],args[1]);
-   System.out.println(merged.name+" is "+merged.description+" and can be had for a mere "+merged.price);
- 
- }
+  public static void main(String[] args){
+    ItemFactory f=new ItemFactory();
+    Item x=f.getItem("dirt");
+    Item y=f.getItem("water");
+    System.out.println(x.name+" is "+x.description+" and can be had for a mere "+x.price);
+    Item merged=f.merge(x,y);
+    System.out.println(merged.name+" is "+merged.description+" and can be had for a mere "+merged.price);
+  }
 }
